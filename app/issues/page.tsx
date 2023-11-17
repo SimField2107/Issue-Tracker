d2d817b95504e65c6e50af8b2379e8e88e03ce44
Seline Cornfield <Seline2107@googlemail.com>
2023-11-17 13:48:31 +0000

    Add Loading skeletons

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,50 +3,48 @@ import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
+import delay from 'delay'; 
+import IssueActions from "./new/IssueActions";
 
 const IssuesPage = async () => {
+    const issues = await prisma.issue.findMany(); 
+    await delay(2000); 
 
-  const issues = await prisma.issue.findMany(); 
-  return (
-    <div>
-
-    <div className='mb-5'>
-    <Button>
-        <Link href="/issues/new"> New Issue </Link>{" "}
-    </Button>   
-    </div>
-      
-      <Table.Root variant='surface'> 
-        <Table.Header> 
-            <Table.Row> 
-                <Table.ColumnHeaderCell> Issue </Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell className='hidden md:table-cell'> Status</Table.ColumnHeaderCell>
-                <Table.ColumnHeaderCell className='hidden md:table-cell'> Created </Table.ColumnHeaderCell>
-
-            </Table.Row>
-        </Table.Header>
-            <Table.Body> 
-                {issues.map(issue => ( 
-                    <Table.Row key={issue.id}> 
-                    <Table.Cell> 
-                        {issue.id}
-                        <div className='block md:hidden'> 
-                        <IssueStatusBadge status={issue.status}/> 
-                        
-                        </div>
-                    </Table.Cell> 
-                    <Table.Cell className='hidden md:table-cell'>
-                        <IssueStatusBadge status={issue.status}/> 
-                    </Table.Cell> 
-                    <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()} </Table.Cell> 
-                    
-                    </Table.Row>
-                ))}
-
-            </Table.Body>
-      </Table.Root>
-    </div>
-  );
-};
+    return (
+      <div>
+  <IssueActions/> 
+        
+        <Table.Root variant='surface'> 
+          <Table.Header> 
+              <Table.Row> 
+                  <Table.ColumnHeaderCell> Issue </Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell className='hidden md:table-cell'> Status</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell className='hidden md:table-cell'> Created </Table.ColumnHeaderCell>
+  
+              </Table.Row>
+          </Table.Header>
+              <Table.Body> 
+                  {issues.map(issue => ( 
+                      <Table.Row key={issue.id}> 
+                      <Table.Cell> 
+                          {issue.id}
+                          <div className='block md:hidden'> 
+                          <IssueStatusBadge status={issue.status}/> 
+                          
+                          </div>
+                      </Table.Cell> 
+                      <Table.Cell className='hidden md:table-cell'>
+                          <IssueStatusBadge status={issue.status}/> 
+                      </Table.Cell> 
+                      <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()} </Table.Cell> 
+                      
+                      </Table.Row>
+                  ))}
+  
+              </Table.Body>
+        </Table.Root>
+      </div>
+    );
+  };
 
 export default IssuesPage;
