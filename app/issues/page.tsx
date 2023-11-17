59720c246d8498e214effd578dcd34fc96c8bc4b
Seline Cornfield <Seline2107@googlemail.com>
2023-11-17 15:03:11 +0000

    Build a styles link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
+import Link from '../components/Link'; 
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from 'delay'; 
@@ -25,7 +25,7 @@ const IssuesPage = async () => {
           </Table.Header>
               <Table.Body> 
                   {issues.map(issue => ( 
-                      <Table.Row key={issue.title}> 
+                      <Table.Row key={issue.id}> 
                       <Table.Cell> 
                         <Link href={`/issues/${issue.id}`}>
                         {issue.title}
