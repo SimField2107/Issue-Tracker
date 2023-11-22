bc4bcd0ef27bcf86833d5368c3e1dd6789c2236a
Seline Cornfield <Seline2107@googlemail.com>
2023-11-22 15:59:57 +0000

    Build the aassignee select component

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,7 @@
+import authOptions from "@/app/auth/authOptions";
 import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 
@@ -7,7 +9,12 @@ import { NextRequest, NextResponse } from "next/server";
 export async function PATCH(
     request: NextRequest,
      {params}: { params: {id: string}}) { 
-       const body = await request.json()
+      const session = await getServerSession(authOptions); 
+      if (!session)
+      return NextResponse.json({}, {status: 401}); 
+
+
+      const body = await request.json()
       const validation =  issueSchema.safeParse(body); 
       if (!validation.success)
       return NextResponse.json(validation.error.format(), {status: 400 }); 
@@ -33,6 +40,10 @@ export async function PATCH(
     export async function DELETE(
         request: NextRequest,
          {params}: { params: {id: string}}) { 
+
+          const session = await getServerSession(authOptions); 
+          if (!session)
+          return NextResponse.json({}, {status: 401}); 
 
         const issue = await prisma.issue.findUnique({ 
             where: { id: parseInt(params.id)}
